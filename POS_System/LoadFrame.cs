209d55e415a08d1d4d38da9d9a0763b06481bb24
Rufelle Emmanuel Pactol <rufelleemmanuelpactol7@gmail.com>
2023-12-20 10:18:42 +0800

    New updates winforms

---
+++ POS_System/LoadFrame.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace POS_System
+{
+    public partial class LoadFrame : Form
+    {
+        public LoadFrame()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ POS_System/WebView.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace POS_System
+{
+    public partial class WebView : Form
+    {
+        public WebView()
+        {
+            InitializeComponent();
+        }
+    }
+}
